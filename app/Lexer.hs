ad675526a7856a4021d45064704021b8cede1747
romes <rodrigo.m.mesquita@gmail.com>
2021-08-22 18:57:23 +0100

    fast finish

--- app/Lexer.hs
+++ app/Lexer.hs
@@ -5,14 +5,15 @@ import qualified Text.Parsec.Token as P
 import Text.Parsec.Language (emptyDef)
 
 lexer = P.makeTokenParser $ emptyDef {
-    P.reservedNames = ["main", "with", "do", "please"],
-    P.reservedOpNames = ["\\", "->", ":", "!", "#", "##"] 
+    P.reservedNames = ["main", "with", "do", "please", "when", "is", "for"],
+    P.reservedOpNames = ["\\", "->", ":", "!", "#", "##", "[", "]", ",", ".", "*", "+", "-"]
                                      }
 parens = P.parens lexer
 integer = P.integer lexer
 identifier = P.identifier lexer
 reserved = P.reserved lexer
 reservedOp = P.reservedOp lexer
+natural = P.natural lexer
 
 contents p = do
   P.whiteSpace lexer
--- app/Parser.hs
+++ app/Parser.hs
@@ -2,9 +2,11 @@
 module Parser where
 
 import Text.Parsec
+import Text.Parsec.Expr
 import Control.Monad
 import Data.Bifunctor
 import Data.List
+import Data.Maybe
 
 import Core
 import Lexer
@@ -18,7 +20,6 @@ pmanydecl = do
         reservedOp "#"
         n <- identifier
         reservedOp "#"
-        -- c <- option Nothing $ Just <$> between (reservedOp "#") (reservedOp "##") (many $ satisfy ('#' /=))
         c <- many $ satisfy ('#' /=)
         reservedOp "##"
         return $ Decl n (Commt c)
@@ -31,17 +32,23 @@ pmanydecl = do
     do
         reserved "main"
         reservedOp ":"
-        (((cs++ls)++) . (:[])) . Decl "main" . Presentation <$> many identifier
+        (((cs++ls)++) . (:[])) . Decl "main" . Presentation <$> many (pvar <|> parens pexp)
 
-pexp :: Parser Expr
+pexp, pexp' :: Parser Expr
 pexp = do
      x <- pnotapp
      (do 
          xs <- many1 pnotapp
          return (foldl App x xs)) <|> return x
+pexp' = do
+     x <- pnotapp'
+     (do 
+         xs <- many1 pnotapp'
+         return (foldl App x xs)) <|> return x
 
-pnotapp, plam, pint, pvar :: Parser Expr
-pnotapp = plam <|> pint <|> pchar <|> pvar <|> parens pexp
+pnotapp, plam, pint, pvar, plist :: Parser Expr
+pnotapp = opexpr <|> pnotapp'
+pnotapp' = plam <|> plist <|> pcaselist <|> pint <|> pchar <|> pvar <|> parens pexp
 
 plam = do
      reserved "with"
@@ -50,10 +57,47 @@ plam = do
      e <- pexp
      reserved "please"
      return $ Lambda n e
-pint = Lit . I <$> integer
+pint = Lit . I <$> natural
 pvar = Var <$> identifier
 pchar = do
     char '\''
     c <- letter
     char '\''
     return $ Lit $ C c
+plist = choice [try $ reservedOp "[" >> reservedOp "]" >> return (List []), do
+    reservedOp "["
+    ls <- many $ do
+        e <- pexp
+        optional $ reservedOp ","
+        return e
+    reservedOp "]"
+    return $ List ls]
+pcaselist = do
+    reserved "when"
+    e0 <- pexp
+    reserved "is"
+    reservedOp "["
+    reservedOp "]"
+    reserved "do"
+    e1 <- pexp
+    reserved "please"
+    reserved "for"
+    n1 <- identifier
+    reservedOp ":"
+    n2 <- identifier
+    reserved "do"
+    e2 <- pexp
+    reserved "please"
+    return $ CaseList e0 e1 n1 n2 e2
+
+opexpr = buildExpressionParser table pexp'
+    where
+         table = [ 
+             [binary ":" Prepend AssocRight] 
+           , [binary "*" (\x y -> Arith $ Mult x y) AssocLeft ]
+           , [binary "+" (\x y -> Arith $ Add x y) AssocLeft, binary "-" (\x y -> Arith $ Sub x y) AssocLeft ]
+           ]
+
+         binary name fun = Infix (reservedOp name >> return fun)
+
+
